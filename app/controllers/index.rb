38c77622b48dfb5b07e01cfb88fae1b2f3381c6f
Apprentice <apprentice@dbc-chicago033.local>
2013-11-07 14:53:52 -0600

    carrierwave and minimagick gems

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,14 @@
+# ===== GET ====================================
+
 get '/' do
   # Look in app/views/index.erb
   erb :index
+end
+
+# ===== POST====================================
+
+post '/albums/:album_id' do
+    photo = current_user.albums.find(params[:album_id]).photos.new()
+    photo.file = params[:image]
+    photo.save
 end
